063f46c706bd6c10f07d569977e9fe45b3eb56db
Julian Qian <jqian@jqian.net>
2010-10-14 02:26:47 +0000

    clean up some functions

--- emacsconf/config/func-prog.el
+++ emacsconf/config/func-prog.el
@@ -21,18 +21,6 @@
         (ywb-insert-paren))
     (error (delete-char -1))))
 
-(defun my-sourcepair-load (arg)
-  "Call `sourcepair-load'.
-With prefix argument, if not found create the file."
-  (interactive "P")
-  (let ((res (sourcepair-load)))
-    (when (and (not (bufferp res)) arg)
-      (let* ((files (sourcepair-analyze-filename (file-name-nondirectory (buffer-file-name))))
-             (file (concat (file-name-as-directory (caar files))
-                           (cadr files))))
-        (find-file file)
-        (message "Create %s" file)))))
-
 ;;{{{  Auto recompile for emacs-lisp
 (defvar auto-recompile nil)
 (put 'auto-recompile 'safe-local-variable 'booleanp)
@@ -55,76 +43,14 @@ With prefix argument, if not found create the file."
 (add-hook 'emacs-lisp-mode-hook 'auto-recompile-save-hook)
 ;;}}}
 
-;;{{{  auto-pair
-(setq skeleton-pair-default-alist
-      '((?( _ ?)) (?\))
-        (?[ _ ?]) (?\])
-        (?{ _ ?}) (?\})
-        (?` _ ?')))
-(defun my-turn-on-pair-insert (&optional alist)
-  "打开自动插入括号. see also `skeleton-pair-alist'"
-  (interactive)
-  (set (make-local-variable 'skeleton-pair-alist)
-       (append alist
-               skeleton-pair-default-alist))
-  (setq skeleton-pair t)
-  (dolist (pair skeleton-pair-alist)
-    (and (and (> (length pair) 1) (integerp (car pair)))
-         (local-set-key (string (car pair)) 'skeleton-pair-insert-maybe))))
-(defun my-toggle-pair-insert ()
-  "切换自动插入括号"
-  (interactive)
-  (setq skeleton-pair (not skeleton-pair)))
-;;}}}
-
 (defun ywb-html-insert-newline ()
   (interactive)
   (insert "<br />"))
 
-(defun bibus-export-bib-1 ()
-  (let ((ref (bibus-ref-at-point)))
-    (format-spec
-     "@ARTICLE{%i,
-  AUTHOR = \"%a\",
-  TITLE = \"%t\",
-  Journal = \"%j\",
-  PAGES = \"%p\",
-  VOLUME = %v,
-  NUMBER = %n,
-  YEAR = %y,
-}
-
-"
-     (format-spec-make
-      ?i (replace-regexp-in-string "#" ":" (bibus-ref-field ref "Identifier"))
-      ?a (replace-regexp-in-string ";" ". and " (bibus-ref-field ref "Author"))
-      ?t (bibus-ref-field ref "Title")
-      ?j (bibus-ref-field ref "Journal")
-      ?p (let ((pages (bibus-ref-field ref "Pages")))
-           (setq pages (split-string pages "-"))
-           (mapconcat 'identity (list (car pages)
-                                      (concat (substring (car pages) 0
-                                                         (- (length (car pages))
-                                                            (length (cadr pages))))
-                                              (cadr pages))) "--"))
-      ?v (bibus-ref-field ref "Volume")
-      ?n (bibus-ref-field ref "Number")
-      ?y (bibus-ref-field ref "Year")))))
-(defun bibus-export-bib ()
-  (interactive)
-  (let (str)
-    (if (bibus-has-marked-refp)
-        (bibus-do-with-marked-ref
-         (lambda ()
-           (setq str (concat str (bibus-export-bib-1)))))
-      (setq str (bibus-export-bib-1)))
-    (if (eq last-command 'kill-region)
-        (kill-append str nil)
-      (kill-new str))
-    (message "export lib")))
 (defun apropos-function (pattern)
   (interactive (list (apropos-read-pattern "function")))
   (apropos-command pattern nil 'fboundp))
+
 (defun ywb-html-preview-region (beg end)
   (interactive "r")
   (let ((file (make-temp-file "region-" nil ".html")))
