4c5678ceef618a0e6b144bf52c5cd811f1931c03
junist <junist@e1784d29-9ee5-0cff-5201-3d777eb8b1eb>
2010-10-19 08:59:28 +0000

    sort files by mtime when call ido.

--- trunk/emacsconf/config/30-elisp.el
+++ trunk/emacsconf/config/30-elisp.el
@@ -197,6 +197,25 @@
                                        nil require-match initial-input hist def))
           ad-do-it))))
 
+  ;; Sort ido filelist by mtime instead of alphabetically
+  (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+  (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+  (defun ido-sort-mtime ()
+    (setq ido-temp-list
+          (sort ido-temp-list
+                (lambda (a b)
+                  (let ((ta (nth 5 (file-attributes
+                                    (concat ido-current-directory a))))
+                        (tb (nth 5 (file-attributes
+                                    (concat ido-current-directory b)))))
+                    (if (= (nth 0 ta) (nth 0 tb))
+                        (> (nth 1 ta) (nth 1 tb))
+                      (> (nth 0 ta) (nth 0 tb)))))))
+    (ido-to-end  ;; move . files to end (again)
+     (delq nil (mapcar
+                (lambda (x) (if (string-equal (substring x 0 1) ".") x))
+                ido-temp-list))))
+
   ;; push the most used directory to `ido-work-directory-list'
   (mapc (lambda (dir)
           (add-to-list 'ido-work-directory-list
