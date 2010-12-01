5f4ec6a71db93cac2b8075386101e3dd43f7ee70
Julian Qian <jqian@jqian.net>
2010-12-01 07:09:56 +0000

    Evernote mode is useful for evernote users, prefix key is C-c e

--- emacsconf/config/30-elisp.el
+++ emacsconf/config/30-elisp.el
@@ -1415,3 +1415,11 @@ If the flag is set, only complete with local files."
     (setq TeX-show-compilation t)
     ))
 
+(deh-require-if 'evernote-mode
+  (executable-find "enclient.rb")
+  (global-set-key "\C-cec" 'evernote-create-note)
+  (global-set-key "\C-ceo" 'evernote-open-note)
+  (global-set-key "\C-ces" 'evernote-search-notes)
+  (global-set-key "\C-ceS" 'evernote-do-saved-search)
+  (global-set-key "\C-cew" 'evernote-write-note)
+  (global-set-key "\C-cep" 'evernote-post-region))
