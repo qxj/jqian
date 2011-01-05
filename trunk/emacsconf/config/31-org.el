4ad06ecace24558de1dc42024c015c1e3bddbec5
junist <junist@e1784d29-9ee5-0cff-5201-3d777eb8b1eb>
2011-01-05 02:39:05 +0000

    Rectify cedet and org settings

--- trunk/emacsconf/config/31-org.el
+++ trunk/emacsconf/config/31-org.el
@@ -1,101 +1,11 @@
-(deh-section-path "org"
+
+(deh-section-path "org-export"
   "~/src/org-7.01h"
   ;; load-path
   (add-to-list 'load-path (expand-file-name "lisp" deh-this-path))
   (add-to-list 'load-path (expand-file-name "contrib/lisp" deh-this-path))
   ;; load required org libraries
   (require 'org-install)
-
-  (setq org-CUA-compatible t)
-  (defun my-find-file-function (file)
-    "find file according to the file extension."
-    (funcall (or (assoc-default file ywb-dired-guess-command-alist
-                                'string-match)
-                 'find-file) file))
-  (deh-add-hook org-load-hook
-    (add-to-list 'org-link-frame-setup
-                 '(file . my-find-file-function)))
-  (deh-add-hook org-mode-hook
-    (local-unset-key "\C-c\C-o")        ; trigger for `org-open-at-point'
-    (org-set-local 'comment-start "#+COMMENT:")
-    (toggle-truncate-lines nil)
-    (auto-fill-mode 1)
-    (outline-minor-mode t))
-
-  ;; org keybinds
-  (deh-local-set-key org-mode-hook
-    ((kbd "C-c o l") . 'org-store-link)
-    ((kbd "C-c o a") . 'org-agenda)
-    ((kbd "C-c o b") . 'org-iswitchb)
-    ((kbd "C-c o r") . 'org-remember)
-    ("\M-\C-l" . 'org-table-sort-lines)
-    ("\M-\C-w" . 'org-table-copy-region)
-    ("\M-\C-y" . 'org-table-paste-rectangle)
-    ("\M-I" . 'org-toggle-iimage-in-org)
-    ;;## Org Keybinds Reminds ;;;;;;;;;;;;;;;;;;;
-    ;; ((kbd "C-c C-b") . 'org-beamer-select-environment)
-    ;; ((kbd "C-c C-x p") . 'org-set-property)
-    ;; ((kbd "C-c /") . 'org-sparse-tree)
-    ;; ((kbd "C-c C-x C-c") . 'org-columns)
-    ;; ((kbd "C-c C-x C-l") . 'org-preview-latex-fragment)
-    ;; ((kbd "C-c C-e") . 'org-export)
-    ;; ((kbd "C-c C-a") . 'org-attach)
-    )
-
-  (defun org-toggle-iimage-in-org ()
-    "Display images in your org file."
-    (interactive)
-    (if (face-underline-p 'org-link)
-        (set-face-underline-p 'org-link nil)
-      (set-face-underline-p 'org-link t))
-    (iimage-mode))
-
-  (deh-section "org-agenda"
-    ;; (setq org-agenda-include-diary t) ; contain calendar
-    ;; (setq org-log-done t)
-    ;; (setq org-log-done 'note) ; completed task notes
-    (setq org-log-done 'time) ; time stamp
-
-    (setq org-hide-leading-stars t
-          org-startup-folded nil)           ; don't fold org items after load
-
-    ;; (setq org-agenda-files my-org-dir) ; cause Shift-Right issue
-
-    (setq org-todo-keywords
-          '((sequence  "TODO(t)"  "WAIT(w@/!)" "START(s!)" "|" "CANCEL(c@/!)" "DONE(d!)")))
-
-    ;; Single keys to execute commands at the beginning of a headline
-    (setq org-use-speed-commands t
-          org-export-with-sub-superscripts nil
-          org-file-apps-defaults-gnu '((t . emacs)))
-
-    ;; Remember Settings
-    ;; (org-remember-insinuate)
-    (setq org-directory my-org-dir
-          org-default-notes-file (concat my-org-dir "Notes.org")
-          org-remember-templates
-          '(("Tasks"  ?t  "* TODO %^{Title} %^g\n       %?     %i\n"
-             "Task.org"  "New task")
-            ("Personal"  ?g  "* %^{Title} %^g\n       %?     %i\n   Reference: %a"
-             "Personal.org"  "New arrangement")
-            ("Journal"  ?d  "* %u %^{Title}\n  %?\n   %i\n\n     Reference: %a"
-             "Journal.org")
-            ("Study"  ?x  "* %u %^{Title}\n  %?\n   %i\n\n     Reference: %a"
-             "Study.org" "New item")
-            ("Project"  ?s  "* %^{Title}\n  %?\n   %i\n\n     Reference: %a"
-             "Project.org"  "New resource")))
-
-    ;; If missing the function org-remember-insinuate, try the following
-    (setq remember-annotation-functions '(org-remember-annotation)
-          remember-handler-functions '(org-remember-handler))
-    (add-hook 'remember-mode-hook 'org-remember-apply-template)
-
-    ;;(require 'org-export-freemind-install)
-
-    (setq org-agenda-sorting-strategy
-          '((agenda priority-down time-up)
-            (todo priority-down category-keep)
-            (tags priority-down category-keep))))
 
   ;;# export org documents to latex & pdf
   (deh-require 'org-latex
@@ -155,8 +65,54 @@
 
     (setq org-export-html-style
           "<link rel=\"stylesheet\" type=\"text/css\" href=\"wheer.css\">"))
+  )
 
-  ;; Compatible with yasnippet.el
+(deh-require 'org
+  (setq org-CUA-compatible t)
+  (defun my-find-file-function (file)
+    "find file according to the file extension."
+    (funcall (or (assoc-default file ywb-dired-guess-command-alist
+                                'string-match)
+                 'find-file) file))
+  (deh-add-hook org-load-hook
+    (add-to-list 'org-link-frame-setup
+                 '(file . my-find-file-function)))
+  (deh-add-hook org-mode-hook
+    (local-unset-key "\C-c\C-o")        ; trigger for `org-open-at-point'
+    (org-set-local 'comment-start "#+COMMENT:")
+    (toggle-truncate-lines nil)
+    (auto-fill-mode 1)
+    (outline-minor-mode t))
+
+  ;; org keybinds
+  (deh-local-set-key org-mode-hook
+    ((kbd "C-c o l") . 'org-store-link)
+    ((kbd "C-c o a") . 'org-agenda)
+    ((kbd "C-c o b") . 'org-iswitchb)
+    ((kbd "C-c o r") . 'org-remember)
+    ("\M-\C-l" . 'org-table-sort-lines)
+    ("\M-\C-w" . 'org-table-copy-region)
+    ("\M-\C-y" . 'org-table-paste-rectangle)
+    ("\M-I" . 'org-toggle-iimage-in-org)
+    ;;## Org Keybinds Reminds ;;;;;;;;;;;;;;;;;;;
+    ;; ((kbd "C-c C-b") . 'org-beamer-select-environment)
+    ;; ((kbd "C-c C-x p") . 'org-set-property)
+    ;; ((kbd "C-c /") . 'org-sparse-tree)
+    ;; ((kbd "C-c C-x C-c") . 'org-columns)
+    ;; ((kbd "C-c C-x C-l") . 'org-preview-latex-fragment)
+    ;; ((kbd "C-c C-e") . 'org-export)
+    ;; ((kbd "C-c C-a") . 'org-attach)
+    )
+
+  (defun org-toggle-iimage-in-org ()
+    "Display images in your org file."
+    (interactive)
+    (if (face-underline-p 'org-link)
+        (set-face-underline-p 'org-link nil)
+      (set-face-underline-p 'org-link t))
+    (iimage-mode))
+
+  ;;# Compatible with yasnippet.el
   (if (featurep 'yasnippet)
       (deh-add-hook org-mode-hook
         (org-set-local 'yas/trigger-key [tab])
@@ -164,6 +120,52 @@
         (define-key yas/keymap (kbd "M-j") 'yas/next-field-or-maybe-expand)))
   )
 
+(deh-section "org-agenda"
+  ;; (setq org-agenda-include-diary t) ; contain calendar
+  ;; (setq org-log-done t)
+  ;; (setq org-log-done 'note) ; completed task notes
+  (setq org-log-done 'time) ; time stamp
+
+  (setq org-hide-leading-stars t
+        org-startup-folded nil)           ; don't fold org items after load
+
+  ;; (setq org-agenda-files my-org-dir) ; cause Shift-Right issue
+
+  (setq org-todo-keywords
+        '((sequence  "TODO(t)"  "WAIT(w@/!)" "START(s!)" "|" "CANCEL(c@/!)" "DONE(d!)")))
+
+  ;; Single keys to execute commands at the beginning of a headline
+  (setq org-use-speed-commands t
+        org-export-with-sub-superscripts nil
+        org-file-apps-defaults-gnu '((t . emacs)))
+
+  ;; Remember Settings
+  ;; (org-remember-insinuate)
+  (setq org-directory my-org-dir
+        org-default-notes-file (concat my-org-dir "Notes.org")
+        org-remember-templates
+        '(("Tasks"  ?t  "* TODO %^{Title} %^g\n       %?     %i\n"
+           "Task.org"  "New task")
+          ("Personal"  ?g  "* %^{Title} %^g\n       %?     %i\n   Reference: %a"
+           "Personal.org"  "New arrangement")
+          ("Journal"  ?d  "* %u %^{Title}\n  %?\n   %i\n\n     Reference: %a"
+           "Journal.org")
+          ("Study"  ?x  "* %u %^{Title}\n  %?\n   %i\n\n     Reference: %a"
+           "Study.org" "New item")
+          ("Project"  ?s  "* %^{Title}\n  %?\n   %i\n\n     Reference: %a"
+           "Project.org"  "New resource")))
+
+  ;; If missing the function org-remember-insinuate, try the following
+  (setq remember-annotation-functions '(org-remember-annotation)
+        remember-handler-functions '(org-remember-handler))
+  (add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+  ;;(require 'org-export-freemind-install)
+
+  (setq org-agenda-sorting-strategy
+        '((agenda priority-down time-up)
+          (todo priority-down category-keep)
+          (tags priority-down category-keep))))
 
 (deh-section "rst"
   (add-hook 'rst-adjust-hook 'rst-toc-update)
