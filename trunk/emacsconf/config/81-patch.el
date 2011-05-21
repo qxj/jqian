57c92c3080cabb0dd000def35566c5a1d7b19e30
junist <junist@e1784d29-9ee5-0cff-5201-3d777eb8b1eb>
2011-05-21 17:06:02 +0000

    Add my-toggle-gnus

--- trunk/emacsconf/config/81-patch.el
+++ trunk/emacsconf/config/81-patch.el
@@ -430,6 +430,5 @@ Using a template for inserting some text consists of:
 (defun my-toggle-mutt ()
   (interactive)
   (if (string-match "*mutt*" (buffer-name))
-      (while (string-match "*mutt*" (buffer-name))
-        (bury-buffer))
+      (bury-buffer)
     (run-in-ansi-term "mutt" t)))
