16ba5544dc55029b5e1dcf943e6ccd058afdede5
Julian Qian <jqian@jqian.net>
2011-05-21 17:06:02 +0000

    Add my-toggle-gnus

--- emacsconf/config/70-gnus.el
+++ emacsconf/config/70-gnus.el
@@ -74,7 +74,8 @@
 (setq gnus-interactive-exit nil
       gnus-subscribe-newsgroup-method 'gnus-subscribe-topics
       gnus-subscribe-options-newsgroup-method 'gnus-subscribe-topics
-      gnus-fetch-old-headers 'some
+      gnus-fetch-old-headers t          ; 'some
+      gnus-nov-is-evil nil
       gnus-use-cross-reference nil
       gnus-gcc-mark-as-read t)
 ;;;; Gnus Parameters
@@ -233,102 +234,23 @@
 ;;
 ;; (setq gnus-use-cache 'passive)
 
-;;
-;; 多窗口处理
-;;
-;;
-;; From: Katsumi Yamaoka <yamaoka@jpl.org>
-;; Subject: Re: multiple message frames
-;; To: ding@gnus.org
-;; Newsgroups: gnus.ding
-;; Date: Thu, 11 Sep 2003 16:06:37 +0900
-;;
-;; Hi,
-;;
-;; This is the revised version of the "multiple message frames" suit.
-;; It makes it possible to open multiple message frames and delete
-;; each frame automatically after sending or killing it.  You can use
-;; it by simply putting it in your .gnus.el file.  Enjoy!
-;;
+(defun my-toggle-gnus ()
+  (interactive)
+  (if (or (string-match "*Group*" (buffer-name))
+          (string-match "*Summary" (buffer-name)))
+      (bury-buffer)
+    (let ((list (buffer-list)))
+      (while list
+        (if (or (string-match "*Group*" (buffer-name (car list)))
+                (string-match "*Summary" (buffer-name (car list))))
+            (progn
+              (switch-to-buffer (car list))
+              (setq list nil))
+          (setq list (cdr list))))
+      (unless (or (string-match "*Group*" (buffer-name))
+                  (string-match "*Summary" (buffer-name)))
+        (call-interactively 'gnus)))))
 
-(let* ((default
-         ;; Winodw layout for normal message frames.
-         '(vertical
-           ((user-position . t)
-            ;;(left . -1) (top . 1)
-            (width . 80) (height . 40))
-           (message 1.0 point)))
-       (bug
-        ;; Window layout for message frames reporting bugs.
-        ;; Note that multiple gnus-bug frames are not supported.
-        '(vertical
-          ((user-position . t)
-           ;;(left . -1) (top . 1)
-           (width . 80) (height . 40))
-          (if gnus-bug-create-help-buffer '("*Gnus Help Bug*" 0.5))
-          ("*Gnus Bug*" 1.0 point)))
-       (config
-        `(frame
-          1.0
-          (if (buffer-live-p gnus-summary-buffer)
-              (if (get-buffer gnus-article-buffer)
-                  (car (cdr (assq 'article gnus-buffer-configuration)))
-                (car (cdr (assq 'summary gnus-buffer-configuration))))
-            (car (cdr (assq 'group gnus-buffer-configuration))))
-          ,default))
-       (settings '(compose-bounce forward mail-bounce message post
-                                  reply reply-yank)))
-  (while settings
-    (gnus-add-configuration (list (car settings) config))
-    (setq settings (cdr settings)))
-  (setcdr (nthcdr 2 (setq config (copy-sequence config))) (list bug))
-  (gnus-add-configuration (list 'bug config)))
-
-(add-hook
- 'gnus-configure-windows-hook
- (lambda nil
-   (if (eq major-mode 'message-mode)
-       (let* ((message-frame (selected-frame))
-              (delete-frame-function
-               `(lambda nil
-                  (if (and
-                       ;; Uncomment the following line if other windows
-                       ;; in message frames are supposed to be important.
-                       ;;(eq (selected-window) (next-window))
-                       (eq (selected-frame) ,message-frame))
-                      (delete-frame ,message-frame)))))
-         (setq gnus-frame-list (delq message-frame gnus-frame-list)
-               message-exit-actions `((funcall ,delete-frame-function))
-               message-postpone-actions message-exit-actions)
-         (if (or (featurep 'xemacs)
-                 (< emacs-major-version 21))
-             (make-local-hook 'kill-buffer-hook))
-         (add-hook 'kill-buffer-hook `,delete-frame-function t t)))))
-
-;; Don't popup a message frame when sending a queued message.
-(add-hook
- 'gnus-message-setup-hook
- (lambda nil
-   (if (or (memq this-command '(gnus-draft-send-message
-                                gnus-draft-send-all-messages
-                                gnus-group-send-queue))
-           (and (featurep 'gnus-delay)
-                (save-excursion
-                  (save-restriction
-                    (widen)
-                    (message-narrow-to-headers)
-                    (re-search-forward
-                     (concat "^" (regexp-quote gnus-delay-header)
-                             ":\\s-+")
-                     nil t)))))
-       (let ((config (copy-sequence gnus-buffer-configuration)))
-         (set (make-local-variable 'gnus-buffer-configuration)
-              (cons '(forward (vertical 1.0 (message 1.0 point)))
-                    (delq (assq 'forward config) config)))
-         (set (make-local-variable 'gnus-configure-windows-hook)
-              nil)))))
-
-;; REF: http://www.ibm.com/developerworks/cn/linux/l-cn-emacsgnus/
 
 ;; (gnus-compile)
 
