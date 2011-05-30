c89f0906556ca2bd509c27c37bb50052e0a27a87
Julian Qian <jqian@jqian.net>
2011-05-30 16:34:23 +0000

    trivial updates

--- emacsconf/config/73-twitter.el
+++ emacsconf/config/73-twitter.el
@@ -1,6 +1,6 @@
 ;; -*- coding: utf-8 mode: Emacs-Lisp -*-
 ;; 73-twitter.el ---
-;; Time-stamp: <Julian Qian 2011-05-30 14:02:59>
+;; Time-stamp: <Julian Qian 2011-05-31 00:33:37>
 ;; Created: 2011 Julian Qian
 ;; Version: $Id: 73-twitter.el,v 0.0 2011/05/19 08:29:04 jqian Exp $
 
@@ -20,7 +20,7 @@
   ;; (setq-default twittering-icon-mode t)
 
   (setq twittering-initial-timeline-spec-string ":home"
-        twittering-api-host "twip/override-mode-url"
+        twittering-api-host "twip/override-mode-url" ; without "http://"!
         twittering-api-search-host twittering-api-host
         twittering-username "jqian"
         twittering-password "password"
