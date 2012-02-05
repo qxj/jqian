e52a20371392f53250a6dc19e96794f56ead28f0
junist <junist@e1784d29-9ee5-0cff-5201-3d777eb8b1eb>
2012-02-05 14:11:02 +0000

    Add progress indicator for sendbook.py

--- trunk/python/kindle/sendbook.py
+++ trunk/python/kindle/sendbook.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# sendbook.py --- Time-stamp: <Qian Julian 2012-02-05 20:49:55>
+# sendbook.py --- Time-stamp: <Qian Julian 2012-02-05 22:10:38>
 # Copyright 2012 Qian Julian
 # Author: junist@gmail.com
 # Version: $Id: sendbook.py,v 0.0 2012/02/05 10:34:09 jqian Exp $
@@ -9,7 +9,8 @@
 Send book to xxx@free.kindle.com with your gmail account
 """
 
-import smtplib, mimetypes, sys, os, pdb
+import mimetypes, sys, os, pdb
+from smtplib import SMTP, quotedata, CRLF, SMTPDataError
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
@@ -19,6 +20,44 @@ KINDLEID = 'xxx@free.kindle.com'
 USERNAME = 'user@gmail.com'
 PASSWORD = 'password'
 
+class ExtendedSMTP(SMTP):
+    def data(self, msg):
+        """
+        This is a modified copy of smtplib.SMTP.data()
+
+        Sending data in chunks and calling self.callback
+        to keep track of progress,
+        """
+        self.putcmd("data")
+        (code,repl)=self.getreply()
+        if self.debuglevel >0 : print>>sys.stderr, "data:", (code,repl)
+        if code != 354:
+            raise SMTPDataError(code,repl)
+        else:
+            q = quotedata(msg)
+            if q[-2:] != CRLF:
+                q = q + CRLF
+            q = q + "." + CRLF
+
+            # begin modified send code
+            chunk_size = 10240
+            bytes_sent = 0
+
+            while bytes_sent != len(q):
+                chunk = q[bytes_sent:bytes_sent+chunk_size]
+                self.send(chunk)
+                bytes_sent += len(chunk)
+                if hasattr(self, "callback"):
+                    self.callback(bytes_sent, len(q))
+            # end modified send code
+
+            (code, msg) = self.getreply()
+            if self.debuglevel > 0 : print>>sys.stderr, "data:", (code,msg)
+            return (code,msg)
+
+def callback(progress, total):
+    print "[%d%%] %d sent of %d bytes" % (progress*100/total, progress, total)
+
 class SendBook(object):
     """
     """
@@ -27,16 +66,17 @@ class SendBook(object):
         self.fromaddr = username
         self.toaddr = kindleid
 
-        self.server = smtplib.SMTP('smtp.gmail.com', 587)
-        self.server.starttls()
-        self.server.login(username, passwd)
+        self.smtp = ExtendedSMTP('smtp.gmail.com', 587)
+        self.smtp.callback = callback
+        self.smtp.starttls()
+        self.smtp.login(username, passwd)
 
     def __enter__(self):
         print 'Begin to send book ...'
         return self
 
     def __exit__(self, type, value, traceback):
-        self.server.quit()
+        self.smtp.quit()
         print 'End(SendBook).'
         return True
 
@@ -56,7 +96,7 @@ class SendBook(object):
         att.add_header('Content-Disposition', 'attachment', filename = filename)
         msg.attach(att)
 
-        self.server.sendmail(self.fromaddr, self.toaddr, msg.as_string())
+        self.smtp.sendmail(self.fromaddr, self.toaddr, msg.as_string())
 
 def main():
     "sendbook.py"
