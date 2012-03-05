c880ee79fc652c61878505c8fabe3921be541d8b
Julian Qian <jqian@jqian.net>
2012-03-05 12:13:37 +0000

    the branch for emacs configration updates

--- samples/Hadoop the Definitive Guide/ch06/src/main/java/NonSplittableTextInputFormat.java
+++
@@ -1,9 +0,0 @@
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.mapred.TextInputFormat;
-
-public class NonSplittableTextInputFormat extends TextInputFormat {
-  @Override
-  protected boolean isSplitable(FileSystem fs, Path file) {
-    return false;
-  }
-}
