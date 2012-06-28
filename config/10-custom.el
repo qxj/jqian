975af1e4558941858eae90b96bfc1e09fbd5bd6b
Julian Qian <jqian@jqian.net>
2012-06-28 05:56:03 +0000

    create an individual branch for emacs23

---
+++ config/10-custom.el
@@ -0,0 +1,218 @@
+;; -*- coding: utf-8 -*-
+
+(setq debug-on-error nil debug-on-quit nil)
+
+;;{{{ Generic Settings
+;; syntax highlight
+(cond ((fboundp 'global-font-lock-mode)
+       ;; Turn on font-lock in all modes that support it
+       (global-font-lock-mode t)
+       ;; Maximum colors
+       (setq font-lock-maximum-decoration t)
+       (setq scalable-fonts-allowed t)))
+(setq font-lock-maximum-size
+      (quote ((t . 1280000) (c-mode . 256000) (c++-mode . 256000))))
+(setq-default default-directory (expand-file-name "~/"))
+;; echo key strokes quickly
+(setq echo-keystrokes 0.1)
+;; auto fill : M-q
+(setq default-justification 'full)
+(setq adaptive-fill-mode nil)
+(setq default-fill-column 72)
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(setq column-number-mode t)
+(setq ring-bell-function 'ignore)
+;; (setq visible-bell t)
+
+(setq system-time-locale "C")
+
+(setq truncate-partial-width-windows nil)
+
+;; highlight trailing whitespace
+(setq show-trailing-whitespace t)
+
+;; Wrap too long lines
+(toggle-truncate-lines nil)
+(setq hscroll-margin 1)
+(setq hscroll-step 1)
+
+;; delete whole line
+(setq-default kill-whole-line t)
+(setq kill-ring-max 50)
+;; indent without tab '\t' but white space
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4)
+(setq tab-stop-list nil)
+(setq display-time-mail-file "~/.emacs.d/mail")
+
+;; chinese charactor at the end of sentences
+(setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*"
+      sentence-end-double-space nil)
+;; scroll-margin, which conflict with pager-mode, thus set to zero
+(setq scroll-margin 0
+      scroll-conservatively 10000)
+;; show matching parentheses
+(show-paren-mode t)
+(setq show-paren-style 'parentheses)
+
+(mouse-avoidance-mode 'animate)
+
+(auto-image-file-mode t)
+;; Highlight selected regions in Gnu Emacs
+(transient-mark-mode t)
+;; Make typing overwrite text selection
+(delete-selection-mode t)
+;; revert buffers automatically when underlying files are changed externally
+;; (global-auto-revert-mode t)
+
+;; Smart indenting and pairing for all (emacs24)
+;; (electric-pair-mode t)
+;; (electric-indent-mode t)
+;; (electric-layout-mode t)
+
+;; (partial-completion-mode 1)
+(icomplete-mode 1)
+(winner-mode 1)
+;; (auto-insert-mode 1)
+
+(deh-section "backup"
+  (setq make-backup-files t
+        version-control t
+        kept-new-versions 3
+        delete-old-versions t
+        kept-old-versions 2
+        dired-kept-versions 1
+        backup-by-copying t)
+  ;; DO NOT depends on the backup, it is not really useful
+  (add-to-list 'backup-directory-alist
+               (cons "." (expand-file-name "backup" my-data-dir)))
+  ;; (setq make-backup-file-name-function
+  ;;       (lambda (fpath)
+  ;;         "Return a new file path of a given file path.
+  ;; If the new path's directories does not exist, create them."
+  ;;         (let* ((backup-root (expand-file-name "backup" my-data-dir))
+  ;;                (bpath (concat backup-root fpath "~")))
+  ;;           (make-directory (file-name-directory bpath) bpath)
+  ;;           bpath)))
+  )
+
+(setq auto-save-list-file-prefix
+      (expand-file-name "emacs.autosave-" my-data-dir))
+
+(setq completion-ignore-case t
+      read-file-name-completion-ignore-case t)
+
+(dolist (ext '(".zip" ".tgz" ".gz" ".tar" ".bz2"
+               ".rpm" ".deb"
+               ".out" ".exe" ".so"))
+  (add-to-list 'completion-ignored-extensions ext))
+
+;; use clipboard
+(setq x-select-enable-clipboard t)
+
+;; show arrow in fringe to indicate boundary of current buffer
+(setq-default indicate-buffer-boundaries 'left)
+
+(setq kill-do-not-save-duplicates t)
+
+(setq tab-always-indent 'complete)
+
+(setq browse-url-browser-function '(("/HyperSpec/" . w3m-browse-url)
+                                    ("." . browse-url-generic))
+      browse-url-generic-program "google-chrome")
+
+(setq find-function-C-source-directory "~/src/emacs-23.2/src/"
+      find-function-source-path '("~/src/emacs-23.2/lisp/"))
+
+;; find-dired
+(setq find-ls-option '("-print0 | xargs -0 ls -ld" . "-ld"))
+
+(deh-section "abbrev"
+  (setq abbrev-file-name (expand-file-name "emacs.abbrev_defs" my-data-dir))
+  (if (file-exists-p abbrev-file-name)
+      (read-abbrev-file abbrev-file-name))
+  (setq save-abbrevs t)
+  (put 'define-abbrev-table 'lisp-indent-function 1))
+
+;; diary, todo, calendar
+(deh-section "calendar"
+  (setq diary-file (expand-file-name "diary" my-org-dir)
+        todo-file-do (expand-file-name "todo-do" my-org-dir)
+        todo-file-done (expand-file-name "todo-done" my-org-dir)
+        todo-file-top (expand-file-name "todo-top" my-org-dir))
+  (deh-add-hook initial-calendar-window-hook (toggle-truncate-lines 1))
+  ;; calendar
+  ;; for calendar-sunrise-sunset
+  (setq calendar-longitude 114
+        calendar-latitude 22.3
+        calendar-location-name "Beijing"))
+
+;; formats and timestamp
+(deh-section "formats"
+  (setq frame-title-format
+        '((:eval
+           (let ((login-name (getenv-internal "LOGNAME")))
+             (if login-name (concat login-name "@") "")))
+          (:eval (system-name))
+          ":"
+          (:eval (or (buffer-file-name) (buffer-name))))))
+
+(deh-section "timestamp"
+  (setq time-stamp-active t
+        time-stamp-warn-inactive t
+        time-stamp-format "%U %:y-%02m-%02d %02H:%02M:%02S"))
+
+;; set my file register
+(deh-section "register"
+  (set-register ?. `(file . ,my-config-dir))
+  (set-register ?b '(file . "~/Dropbox/"))
+  (set-register ?t '(file . "~/temp/"))
+  (set-register ?s '(file . "~/src/"))
+  (set-register ?w '(file . "~/works/"))
+  (set-register ?d '(file . "~/Desktop/")))
+
+;; prevent no response if click the memu in File
+(deh-section "printer"
+  (fset 'print-buffer 'ignore)
+  (setq lpr-command "")
+  (setq printer-name ""))
+;;;}}}
+
+;;{{{ Hooks
+;; no trailing whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; update copyright
+(add-hook 'before-save-hook 'copyright-update)
+;; update timestamp
+(add-hook 'before-save-hook 'time-stamp)
+;;}}}
+
+;;{{{ Customized keywords
+(dolist (mode '(c-mode c++-mode java-mode lisp-mode emacs-lisp-mode python-mode
+                       php-mode lisp-interaction-mode sh-mode sgml-mode))
+  (font-lock-add-keywords
+   mode
+   '(("\\<\\(FIXME\\|TODO\\|BUG\\|HACK\\|WORKAROUND\\|DEPRECATED\\):" 1 font-lock-warning-face prepend)
+     ("\\<\\(DONE\\|NOTE\\):" 1 font-lock-doc-face t)
+     ;; highlight too long lines
+     ;; ("^[^\n]\\{120\\}\\(.*\\)$" 1 font-lock-warning-face t)
+     ;; highlight parentheses
+     ;; ("(\\|)\\|\\[\\|]\\|<\\|>\\|{\\|}" . font-lock-builtin-face)
+     ;; hightlight numbers
+     ("\\<\-?[0-9]*\\.?[0-9]+\\>" . font-lock-constant-face))))
+;;}}}
+
+;; Turn on the features disabled default
+(setq disabled-command-function nil)
+
+(setq tooltip-use-echo-area nil)
+(setq folding-folding-on-startup nil)
+
+(setq user-full-name "Julian Qian"
+      user-mail-address "junist@gmail.com")
+
+;;; customization
+(setq custom-file (expand-file-name "emacs.custom.el" my-data-dir))
+(load custom-file t)
---
+++ config/11-port.el
@@ -0,0 +1,195 @@
+;; -*- coding: utf-8 -*-
+;; This config is for portable. The platform relate configuration
+;; should appear here.
+
+(defvar my-include-dirs
+  (let (dirs
+        (incs '("include" "inc" "common" "public" "hdr"))
+        (updirs '("./" "../" "../../" "../../../" "../../../../")))
+    (dolist (dir updirs)
+      (setq dirs (append dirs (mapcar (lambda (x) (concat dir x)) incs))))
+    (append updirs dirs)))
+
+(deh-section "env"
+  (setenv "GIT_PAGER" "cat")
+  (setenv "PAGER" "cat")
+  (setenv "EDITOR" "emacsclient")
+  )
+
+(deh-section "coding-system"
+  (if (eq system-type 'windows-nt)
+      (progn
+        (setq file-name-coding-system 'gbk)
+        (setq locale-coding-system 'gbk)
+        (set-selection-coding-system 'gbk)
+        (set-terminal-coding-system 'gbk)
+        (set-keyboard-coding-system 'gbk)
+        (set-clipboard-coding-system 'gbk)
+        (set-buffer-file-coding-system 'gbk)
+        (modify-coding-system-alist 'process "*" 'gbk)
+        (setq default-process-coding-system '(gbk . gbk))
+        ;; (set-language-environment 'Chinese-GB)
+        (setq w32-charset-info-alist
+              (cons '("gbk" w32-charset-gb2312 . 936)
+                    w32-charset-info-alist)))
+    (setq file-name-coding-system 'utf-8-unix)
+    (setq locale-coding-system 'utf-8-unix)
+    (set-selection-coding-system 'utf-8-unix)
+    (set-terminal-coding-system 'utf-8-unix)
+    (set-keyboard-coding-system 'utf-8-unix)
+    (set-clipboard-coding-system 'utf-8-unix)
+    (set-buffer-file-coding-system 'utf-8-unix)
+    (modify-coding-system-alist 'process "*" 'utf-8-unix)
+    (setq default-process-coding-system '(utf-8-unix . utf-8-unix)))
+
+  (unless (coding-system-p 'gbk)
+    (define-coding-system-alias 'gbk 'chinese-iso-8bit))
+  (unless (coding-system-p 'chinese-gbk)
+    (define-coding-system-alias 'chinese-gbk 'chinese-iso-8bit))
+  (prefer-coding-system 'utf-8)
+  (add-to-list 'auto-coding-alist '("\\.nfo\\'" . cp437))
+  (dolist (char (append "、。．，·ˉˇ¨〃々―～‖…’”）〕〉》""」』〗】"
+                        "；：""？！""±×÷∶°′″℃／＼＂＿￣｜ㄥ"  nil))
+    (modify-syntax-entry char "." (standard-syntax-table))))
+
+(deh-section "PATH"
+  ;; add more directory to environment variable PATH and exec-path
+  (let ((path (split-string (getenv "PATH") path-separator)))
+    (mapc (lambda (p)
+            (setq p (convert-standard-filename
+                     (expand-file-name p)))
+            (add-to-list 'exec-path p)
+            (add-to-list 'path p))
+          (append
+           '("~/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/texbin" "/usr/X11/bin")
+           (when (eq system-type 'windows-nt)
+             '("d:/programs/emacs/bin" "d:/cygwin/bin" "d:/cygwin/usr/bin"))))
+    (setenv "PATH" (mapconcat 'identity path path-separator))))
+
+(deh-section-if "win32"
+  (eq system-type 'windows-nt)
+  (setq abbreviated-home-dir nil)
+  (setenv "SHELL" "d:/cygwin/bin/bash.exe")
+  (setq explicit-shell-file-name "bash.exe")
+  (setq ffap-c-path '("d:/Programs/MSYS/mingw/include/"
+                      "d:/Programs/MSYS/mingw/include/c++/3.4.0/"))
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              (comint-send-string (get-buffer-process (current-buffer)) "set PERLIO=:unix\n"))))
+
+(deh-section-if "linux"
+  (eq system-type 'gnu/linux)
+  (make-variable-buffer-local 'compile-command)
+
+  (dolist (dir '("/usr/lib/info"
+                 "/usr/gnu/info"
+                 "/usr/gnu/lib/info"
+                 "/opt/gnu/info"
+                 "/usr/share/lib/info"
+                 "/usr/local/share/lib/info"
+                 "/usr/gnu/lib/emacs/info"))
+    (add-to-list 'Info-default-directory-list dir))
+  )
+
+(deh-section-if "macosx"
+  (eq system-type 'darwin)
+  (setq ns-command-modifier 'meta)
+  (dolist (dir '("/usr/include/c++/v1"))
+    (add-to-list 'my-include-dirs dir))
+  ;; fix launching from spotlight
+  ;; $ cat > $HOME/.launchd.conf
+  ;; setenv PATH /usr/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/texbin
+  (setq browse-url-generic-program "open"))
+
+(deh-section "window-system"
+  (cond ((eq window-system 'x)
+         (setq x-select-enable-clipboard t)
+         ;; fix issue that ibus input method issue cannot be started in emacs
+         ;; firstly, $ sudo apt-get install ibus-el
+         (deh-try-require 'ibus
+           (add-hook 'after-init-hook 'ibus-mode-on))
+
+         (setq my-dired-guess-command-alist
+               '(("acroread" "pdf")
+                 ("evince" "pdf")
+                 ;; ("xpdf" "pdf")
+                 ("xdvi" "dvi")
+                 ("dvipdf" "dvi")
+                 ("zxpdf" "pdf.gz")
+                 ("ps2pdf" "ps" "eps")
+                 ("gv" "ps" "eps")
+                 ("unrar x" "rar")
+                 ("kchmviewer" "chm")
+                 ("mplayer -stop-xscreensaver" "avi" "mpg" "rmvb" "rm" "flv" "wmv" "mkv")
+                 ("mplayer -playlist" "list")
+                 ("display" "gif" "jpeg" "jpg" "tif" "png" )
+                 ("eog" "gif" "jpeg" "jpg" "tif" "png")
+                 ("docview.pl" "doc")
+                 ("ooffice -writer" "ods" "doc")
+                 ("ooffice -calc"  "xls")
+                 ("ooffice -impress" "odt" "ppt")
+                 ("gnumeric" "xls")
+                 ("7z x" "7z")
+                 ("djview" "djvu")
+                 ("perl" "pl")
+                 ("firefox" "xml" "html" "htm" "mht"))))
+        (t
+         (setq my-dired-guess-command-alist nil)))
+
+  ;; If terminal and X is sharing the same emacs server, color-theme
+  ;; will affect terminal display. Below function will resolve this
+  ;; issue.
+  (defun init-window-frame (&optional frame)
+    (and frame (select-frame frame))
+    (set-variable 'color-theme-is-global nil)
+    ;; only enable color theme in window system
+    ;; the same color-theme  looks bad in terminal
+    (when window-system
+      (load (expand-file-name "my-fontset.el" my-config-dir))
+      (load (expand-file-name "my-theme.el" my-config-dir))
+      ;; no scroll bar
+      (set-scroll-bar-mode nil)
+      ;; no tool bar
+      (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+      ;; transparent frame
+      (set-frame-parameter (selected-frame) 'alpha '(95 85))
+      (add-to-list 'default-frame-alist '(alpha 95 85))
+      ;; for the height, subtract a couple hundred pixels from the
+      ;; screen height (for panels, menubars and whatnot), then divide
+      ;; by the height of a char to get the height we want
+      (add-to-list 'default-frame-alist
+                   (cons 'height (/ (- (x-display-pixel-height) 100)
+                                    (frame-char-height))))))
+  (add-hook 'after-make-frame-functions 'init-window-frame)
+  (add-hook 'after-init-hook 'init-window-frame))
+
+;; (deh-add-hook 'find-file-hook
+;;   (if (and (buffer-file-name)
+;;            (string-match (expand-file-name "~/src/") (buffer-file-name)))
+;;       (toggle-read-only 1)))
+
+;; post setting
+(when (executable-find "gcc")
+  (setq my-include-dirs (append (gcc-include-path) my-include-dirs)))
+
+(let ((autoload-file (expand-file-name "100-loaddefs.el" my-config-dir)))
+  (unless (file-exists-p autoload-file) (my-generate-loaddefs t autoload-file)))
+
+;; WORKAROUND: miss define-fringe-bitmap under terminal
+(when (null window-system)
+  (unless (fboundp 'define-fringe-bitmap)
+    (defun define-fringe-bitmap (bitmap bits &optional height width align)))
+  (unless (fboundp 'x-hide-tip)
+    (defun x-hide-tip ())))
+
+;; WORKAROUND: skip existed server-name
+(eval-after-load "server"
+  '(let ((file (expand-file-name server-name (if server-use-tcp
+                                                 server-auth-dir
+                                               server-socket-dir))))
+     (if (file-exists-p file)
+         (setq server-name (format "%s.%d" server-name (emacs-pid))))))
+
+;; WORKAROUND: avoid eieio error in ede.el
+(eval-after-load "ede"
+  '(defun ede-customize-forms-menu (menu-def) ()))
